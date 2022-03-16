a9b24959b94ea2f9d9cea7cfbdbb6df8235acc83
SolovevArtem <soloviev.artyomemail@yandex.ru>
2022-03-16 18:25:50 +0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <HeaderComp> </HeaderComp>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from "./components/HeaderComp.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeaderComp,
+  },
+};
 </script>
 
 <style>
@@ -19,8 +20,24 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  background: #f1eae4;
+}
+
+.container {
+  max-width: 1307px;
+  margin: 0px auto;
+  border: 2px dashed black;
+  
+}
+
+.wrapper {
+  min-height: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  
 }
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,67 @@
+<template>
+  <header class="HeaderComp">
+    <div class="container">
+      <div class="HeaderComp__row">
+        <nav class="HeaderComp__nav_left nav">
+          <a class="nav__item shopnow">SHOP NOW</a>
+          <a class="nav__item lookbook">LOOKBOOK</a>
+        </nav>
+        <div class="nav__brand">Felis</div>
+        <nav class="HeaderComp__nav_right nav">
+          <a class="nav__item search">SEARCH</a>
+          <a class="nav__item cart">CART</a>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComp",
+};
+</script>
+
+<style scoped>
+.HeaderComp {
+}
+.HeaderComp__container {
+}
+.HeaderComp__row {
+  display: flex;
+  justify-content: space-between;
+}
+.HeaderComp__nav_left {
+   
+    
+    
+}
+.HeaderComp__nav_left > .lookbook {
+   margin-left: 15px;
+  
+    
+}
+.nav {
+}
+.nav__item {
+    
+}
+.nav__brand {
+  font-family: "Selna";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 52.9025px;
+  line-height: 90px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  margin-right: 388px;
+  margin-left: 363px;
+}
+.HeaderComp__nav_right {
+}
+
+.HeaderComp__nav_right > .search{
+   margin-right: 30px;
+}
+</style>
