bd66152654de277bddf3b946bb9bac5b87a2cb55
SolovevArtem <soloviev.artyomemail@yandex.ru>
2022-04-04 23:27:19 +0300

    Working on email component

--- src/components/InstagramComp.vue
+++ src/components/InstagramComp.vue
@@ -1,15 +1,22 @@
 <template>
-    <div class="InstagramCom">
-
+  <div class="InstagramCom">
+    <div class="container">
+      <div class="inst__Row">
+        <div class="inst__topbox">
+          <div class="inst__title title"></div>
+        </div>
+        <div class="inst__botbox">
+          <img class="img1" src="../assets/Rectangle7.png" alt="" />
+          <img class="img2" src="../assets/Rectangle8.png" alt="" />
+          <img class="img3" src="../assets/Rectangle9.png" alt="" />
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/SignUpComp.vue
+++ src/components/SignUpComp.vue
@@ -1,15 +1,50 @@
 <template>
-    <div>
-
+  <div class="SignUpComp">
+    <div class="container">
+      <div class="signUp__row">
+        <div class="signUp__star">
+          <img class="star" src="../assets/Polygon2.svg" alt="" />
+        </div>
+        <div class="signUp__title pgTitle">SIGN UP FOR OUR NEWSLETTER</div>
+        <div class="signUp__emailbox">
+          <form action="/action_page.php">
+            <label for="email">Enter your email:</label>
+            <input type="email" id="email" name="email" />
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
-
 <script>
-    export default {
-        name: "SignUpComp"
-    }
+export default {
+  name: "SignUpComp",
+};
 </script>
 
 <style scoped>
-
+.SignUpComp {
+  margin-top: 50px;
+}
+.container {
+}
+.signUp__row {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.signUp__star {
+}
+.star {
+  margin-top: 152px;
+}
+.signUp__title {
+}
+.signUp__emailbox {
+}
+.signUp__email {
+}
+.signUp__subscribe {
+}
 </style>
